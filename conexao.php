bf66db6befd0db66f151998175910b5ed6e82259
GABRIEL FERNANDES MATOZINHOS <gabriel.matozinhos@aluno.ifsp.edu.br>
2024-09-02 04:25:16 +0000

    Projeto com as verificações :)

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+// Verifica o local de execução do script PHP
+if ( realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    // Quando entrar nessa condição, significa que o usuário tentou acessar o link diretamente    
+    // Faça algo.
+    die();        
+}
+
+$severname = "localhost";
+$username = "root";  
+$password = "";
+$dbName = "banco_coegi";
+$porta = "3306";
+
+
+$conn = mysqli_connect($severname,$username,$password,$dbName,$porta);
+?>
